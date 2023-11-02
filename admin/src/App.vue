fcb9692f20abe8f436c10e8d878f602df520373b
wusandao <wusandao@163.com>
2023-11-02 15:32:30 +0800

    refactor：fe 删除无用代码

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 import axios from "axios"
 const loginForm = {
   username: 'admin',
@@ -15,20 +14,6 @@ const login = () => {
 
 <template>
   <button @click="login">登录</button>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
 </template>
 
 <style scoped>

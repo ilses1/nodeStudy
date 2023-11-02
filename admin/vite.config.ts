d6debc427084a2eb8e6a6929af42765add719821
wusandao <wusandao@163.com>
2023-11-02 15:16:16 +0800

    chore：fe修改重定向地址

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   	// https: true,
     proxy: {
       '/adminapi': { // 配置需要代理的路径 --> 这里的意思是代理http://localhost:80/api/后的所有路由
-        target: 'http://localhost:3000/', // 目标地址 --> 服务器地址
+        target: 'http://localhost:3000', // 目标地址 --> 服务器地址
         changeOrigin: true, // 允许跨域
       }
      },

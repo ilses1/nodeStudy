fdfc35ea6cd9ad8a9b9b1658542d4a97c1f1c238
wusandao <wusandao@163.com>
2023-11-02 15:20:47 +0800

    feat：BE登录成功返回token

--- server/controllers/admin/UserController.js
+++ server/controllers/admin/UserController.js
@@ -2,6 +2,7 @@
  * @description
  */
 
+const jwt = require('../../utils/JWT')
 const UserService = require("../../services/UserService")
 
 const UserController = {
@@ -14,12 +15,35 @@ const UserController = {
                 error: '用户名或密码错误'
             })
         } else {
+            // 登录成功后，生成token返回token
+            const token = JWT.generateToken({
+                _id: result[0].id,
+                username: result[0].username
+            })
+            // token放入请求头
+            res.headers('Authorization',token)
             res.send({
                 code: '0',
                 actionType: 'ok'
             })
         }
     }
+    // login: async (req, res) => {
+    //     console.log(req.body,'req.body')
+    //     //req.body 
+    //     var result = await UserService.login(req.body)
+
+    //     if (result.length === 0) {
+    //         res.send({
+    //             code: "-1",
+    //             error: "用户名密码不匹配"
+    //         })
+    //     } else {
+    //         res.send({
+    //             ActionType: "OK"
+    //         })
+    //     }
+    // }
 }
 
 module.exports = UserController

813325635cc97b10d5c73d6ec216ea31c111e8d3
wusandao <wusandao@163.com>
2023-11-06 15:01:58 +0800

    feat：fe pinia 设置

---
+++ admin/src/stores/index.ts
@@ -0,0 +1,31 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import type { UserType } from '@/types/user'
+export const useStore = defineStore('counter', () => {
+  const isGetterRouter = ref(false)
+  const isCollapsed = ref(false)
+  const userInfo = ref({
+  } as UserType)
+
+  const changeGetterRouter = (value: boolean) => {
+    isGetterRouter.value = value
+  }
+  const changeCollapsed = () => {
+    isCollapsed.value = !isCollapsed.value
+  }
+  const changeUserInfo = (value: object) => {
+    Object.assign({}, userInfo.value, value)
+  }
+  const clearUserInfo = () => {
+    userInfo.value = {} as UserType
+  }
+
+
+  return {
+    isGetterRouter, isCollapsed, userInfo,
+    changeCollapsed,
+    changeGetterRouter,
+    changeUserInfo,
+    clearUserInfo
+  }
+})

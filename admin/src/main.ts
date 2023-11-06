4c89dc1445b3a890c8e6f79d4571ea7913749c30
wusandao <wusandao@163.com>
2023-11-06 14:59:43 +0800

    feat：fe 修改main.ts 增加新功能

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -5,11 +5,23 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import Particles from "particles.vue3";
 import "./utils/aixos.config"
 
+const pinia = createPinia()
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+// app.use(createPinia())
 
-app.mount('#app')
+app.
+    use(router).
+    use(ElementPlus).
+    use(Particles).
+    use(pinia).
+    mount('#app')
+
+
+
+
--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -7,20 +7,24 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
+
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   server: {
-  	// https: true,
+    // https: true,
     proxy: {
       '/adminapi': { // 配置需要代理的路径 --> 这里的意思是代理http://localhost:80/api/后的所有路由
         target: 'http://localhost:3000', // 目标地址 --> 服务器地址
         changeOrigin: true, // 允许跨域
       }
-     },
+    },
   },
+
+
 
 })

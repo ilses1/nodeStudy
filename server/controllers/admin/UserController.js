dcae882d6c16ef6f2d79c949821031a9ead7c7ae
wusandao <wusandao@163.com>
2023-11-07 14:42:44 +0800

    fix：be bug 登录失败问题

--- server/controllers/admin/UserController.js
+++ server/controllers/admin/UserController.js
@@ -24,7 +24,7 @@ const UserController = {
 
             res.send({
                 code: '0',
-                ActionType: 'ok'
+                ActionType: 'OK'
             })
         }
     },
